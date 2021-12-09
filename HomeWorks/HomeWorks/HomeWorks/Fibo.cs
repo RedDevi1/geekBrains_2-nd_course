b447cc0c4a2f56d5055d4039d4cf973fbcec2034
Nikita <nickita.sevsk@gmail.com>
2021-12-09 18:43:49 +0300

    Lesson_1

---
+++ HomeWorks/HomeWorks/HomeWorks/Fibo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorks
+{
+    public class Fibo
+    {
+        public int Num { set; get; }
+        public Fibo (int num)
+        {
+            num = Num;
+        }
+        public int FiboByCycle (int number)
+        {
+            int num1, num2;
+            num1 = 0; 
+            num2 = 1;
+            int fiboNum = 0;
+            if (number == 1)
+                fiboNum = num1;
+            else if (number == 2)
+                fiboNum = num2;
+            for (int i = 1; i < number; i++)
+            {
+                fiboNum = num1 + num2;
+                num1 = num2;
+                num2 = fiboNum;
+            }
+            return fiboNum;
+        }
+        public int FiboByRecurse(int number)
+        {
+            if (number == 0)
+                return 0;
+            if (number == 1 || number == 2)
+                return 1;
+            return FiboByRecurse(number - 1) + FiboByRecurse(number - 2);
+        }
+    }
+}
---
+++ HomeWorks/HomeWorks/HomeWorks/Lesson_1/Exercise_1/SimpleNum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorks.Lesson_1.Exercise_1
+{
+	public class SimpleNum
+	{
+		public double Num { set; get; }
+		public SimpleNum(double number)
+		{
+			number = Num;
+		}
+		public bool IsSimpleNum (double num)
+        {
+			bool result = false;
+			var d = 0;
+			var i = 2;
+			while (i < num)
+			{
+				if (num % i == 0)
+                {
+					d++;
+					i++;
+				}
+				else
+					i++;
+			}
+			if (d == 0)
+				result = true;
+			return result;			
+		}		
+	}	
+}
